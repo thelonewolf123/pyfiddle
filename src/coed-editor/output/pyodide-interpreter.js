28ca0e7fe5632d468673b871291a5b53430c0159
thelonewolf123 <harishkumar.vellore@gmail.com>
2022-11-21 22:19:17 +0530

    fix

--- src/coed-editor/output/pyodide-interpreter.js
+++ src/coed-editor/output/pyodide-interpreter.js
@@ -1,4 +1,11 @@
 class PyodideInterpreter {
+
+    constructor() {
+        this.pyodideWorker = new Worker("/js/pyodide-worker.js")
+        this.debugLine = null
+        this.inputValue = null
+    }
+
     on(eventName, callback) {
         console.log(eventName, callback)
     }
