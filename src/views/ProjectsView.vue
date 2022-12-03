8864b2f4c3e0dd5a9e42da32eb60dc431a93ed7a
thelonewolf123 <harishkumar.vellore@gmail.com>
2022-12-03 12:07:55 +0530

    added project view

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <h1>Projects</h1>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          name: "hello, world",
+          description: "Simple hello world program in python",
+          author: "Harish kumar",
+          createdTime: Date.now() - 100000,
+          lastUpdated: Date.now(),
+        },
+      ],
+    };
+  },
+};
+</script>
